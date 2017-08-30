d61f2f10ae71e7eaddd520c541ba6fc7300a4778
qpidthc <qpid0421@gmail.com>
2017-08-30 10:35:10 +0800

    Add files via upload

---
+++ DataCenter/WebTHCAPP/App_Start/Startup.Auth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+
+namespace WebTHCAPP
+{
+    public partial class Startup
+    {
+        // 如需設定驗證的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            
+        }
+    }
+}
