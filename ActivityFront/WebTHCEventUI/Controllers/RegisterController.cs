8e5dc642a8e78cc9e4d0a08d15467e85f0ce276e
qpidthc <qpid0421@gmail.com>
2017-08-30 10:17:33 +0800

    Add files via upload

---
+++ ActivityFront/WebTHCEventUI/Controllers/RegisterController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTHCEventUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult newAccount()
+        {
+            var reqMail = Request.Form["mail"];
+            var reqPwd = Request.Form["pwd"];
+            var reqMobil = Request.Form["mobil"];
+            string strAccount = null;
+
+            if (!String.IsNullOrEmpty(reqMail))
+            {
+                strAccount = reqMail;
+            }
+            else if (!String.IsNullOrEmpty(reqMobil))
+            {
+                strAccount = reqMobil;
+            }
+           
+            THC_Library.Error error;
+            long timeStamp;
+
+            
+            Models.Member member = new Models.Member();
+            int newKey = member.newAccount(strAccount, reqMail, reqMobil, reqPwd, out timeStamp, out error);
+
+            string retJson;
+            if (error != null)
+            {
+                retJson = Newtonsoft.Json.JsonConvert.SerializeObject(error);
+            }
+            else
+            {
+                retJson = string.Format("[{{ \"NewId\" : {0}, \"TICKET\" : \"{1}\" , \"ACC\" : \"{2}\" }} ]",
+                                newKey, timeStamp, strAccount);
+            }
+
+            return Json(retJson, "application/json", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult verifyAccount()
+        {
+            var reqMail = Request.Form["mail"];
+            var reqPwd = Request.Form["pwd"];
+
+            THC_Library.Error error;
+
+            Models.Member member = new Models.Member();
+            long lgTimestamp = member.verifyAccount(reqMail, reqPwd, out error);
+
+            string retJson;
+            if (error != null)
+            {
+                retJson = Newtonsoft.Json.JsonConvert.SerializeObject(error);
+            }
+            else
+            {
+                if (lgTimestamp > -1)
+                {
+                    retJson = string.Format("[{{ \"VERIFY\" : 1 , \"TICKET\" : \"{0}\" , \"ACC\" : \"{1}\" }} ]", 
+                        lgTimestamp, reqMail);
+                }
+                else
+                {
+                    retJson = "[{ \"VERIFY\" : 0 , \"MESSAGE\" : \"登入錯誤\"} ]";
+                }                
+            }
+            
+            return Json(retJson, "application/json", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult verifyAccount_FB()
+        {           
+            string regActivity = Request.Form["ac"];
+            string regCode = Request.Form["code"];
+            string regMail = Request.Form["fb"];
+            string regName = Request.Form["name"];
+            string regGender = Request.Form["gender"];
+
+            THC_Library.Error error;
+            string strAccount;
+            Models.Member member = new Models.Member();
+            long lgTimestamp = member.verifyFaceBookAccount(regActivity, regCode, regMail, regName, regGender, out strAccount, out error);
+
+            string retJson;
+            if (error != null)
+            {
+                retJson = Newtonsoft.Json.JsonConvert.SerializeObject(error);
+            }
+            else
+            {
+                if (lgTimestamp > -1)
+                {                   
+                    retJson = string.Format("[{{ \"VERIFY\" : 1 , \"TICKET\" : \"{0}\" , \"ACC\" : \"{1}\" }} ]",
+                        lgTimestamp, strAccount);
+                }
+                else
+                {
+                    retJson = "[{ \"VERIFY\" : 0 , \"MESSAGE\" : \"登入錯誤\"} ]";
+                }
+            }
+
+            return Json(retJson, "application/json", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult renewAccountInfo()
+        {
+            var reqMail = Request.Form["ml"];
+            var reqTicket = Request.Form["tk"];
+            var reqMobil = Request.Form["m"];
+            var reqGender = Request.Form["g"];
+            var reqAge = Request.Form["a"];
+            var reqIId = Request.Form["iid"];
+            var reqAddr = Request.Form["addr"];
+                       
+                       
+            THC_Library.Error error;
+
+            Models.Member member = new Models.Member();
+            int iRowCount = member.updateAccount(reqMail, reqTicket, reqMobil, reqGender, 
+                                reqAge, reqIId, reqAddr, out error);
+
+            string retJson;
+            if (error != null)
+            {
+                retJson = Newtonsoft.Json.JsonConvert.SerializeObject(error);
+            }
+            else
+            {
+                if (iRowCount > 0)
+                {
+                    retJson = string.Format("[{{ \"VERIFY\" : 1, \"COUNT\" : \"{0}\" }} ]",
+                                    iRowCount);
+                }
+                else
+                {
+                    retJson = "[{ \"VERIFY\" : 0 , \"MESSAGE\" : \"資料未更新錯誤\"} ]";
+                }
+            }
+
+            return Json(retJson, "application/json", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ActivityFront/WebTHCEventUI/Controllers/THCController.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+
+namespace WebTHCEventUI.Controllers
+{
+    public class THCController : Controller
+    {
+       
+        // GET: THC
+        public ActionResult Index(string ac, string code)
+        {
+            //if (!THC_Library.RequestChecker.IsMobil(Request.ServerVariables["HTTP_USER_AGENT"]))
+            //{
+            //    return View();
+            //}
+            //else
+            {
+                if (string.IsNullOrEmpty(ac) || string.IsNullOrEmpty(code))
+                {
+                    ViewBag.Message = "";
+                    ViewBag.MessageInfo = "";// string.Format("{0} {1}", ac, code);
+                    return View("outOfPage");
+                }
+                else
+                {
+                    ViewBag.ACTIVITY = ac;
+                    ViewBag.CODE = code;
+                    return View();
+                }
+            }           
+        }
+
+        public ActionResult preGo(string ac,  string code)
+        {
+            //if (!THC_Library.RequestChecker.IsMobil(Request.ServerVariables["HTTP_USER_AGENT"]))
+            //{
+            //    return View();
+            //}
+            //else
+            {
+                if (string.IsNullOrEmpty(ac) || string.IsNullOrEmpty(code))
+                {
+                    return View("outOfPage");
+                }
+
+                THC_Library.Error error;
+
+                 Models.CodeRender codeRender = new Models.CodeRender();
+                 codeRender.checkActivityAndCode(ac, code, out error);
+                
+                 if (error != null)
+                 {
+                     switch (error.Number)
+                     {
+                         case THC_Library.CodeRenderException.ACTIVITY_FINISHED:
+                             ViewBag.Message = "活動已結束";
+                             ViewBag.MessageInfo = error.ErrorMessage;
+                             return View("outOfPage");
+
+                         case THC_Library.CodeRenderException.ACTIVITY_NOT_START:
+                             ViewBag.Message = "活動尚未開始";
+                             ViewBag.MessageInfo = error.ErrorMessage;
+                             return View("outOfPage");
+
+                         case THC_Library.CodeRenderException.INVAILD_ACTIVITY:
+                             ViewBag.Message = "無效的活動";
+                             break;
+                         case THC_Library.CodeRenderException.INVAILD_CODE:
+                             ViewBag.Message = "無效的碼號";
+                             break;
+                         //return View("wrongPage");                       
+                         case THC_Library.CodeRenderException.REPEAT_SCAN:
+                             ViewBag.Message = "已掃描";
+                             string[] strSplit =  error.ErrorMessage.Split(' ');
+                             ViewBag.RepeatDate = strSplit[0];
+                             ViewBag.RepeatTime = strSplit[1];
+                             return View("repeatPage");
+                         case THC_Library.CodeRenderException.SYSTEM_ERROR:
+                             break;
+                     }
+                     return View("wrongPage");
+                 }
+                 else
+                 {
+                     ViewBag.ACTIVITY = ac;
+                     ViewBag.CODE = code;
+                     return View("index");
+                 }               
+            }
+           
+        }
+
+        public ActionResult preLogin(string ac, string code)
+        {
+            ViewBag.ACTIVITY = ac;
+            ViewBag.CODE = code;
+            return View("preLoginPage");
+        }
+
+        public ActionResult login(string ac, string code)
+        {
+            ViewBag.ACTIVITY = ac;
+            ViewBag.CODE = code;
+            return View("loginPage");
+        }
+             
+        public ActionResult regAccount(string ac, string code)
+        {
+            ViewBag.ACTIVITY = ac;
+            ViewBag.CODE = code;
+            return View("regAccount");
+        }
+        
+        public ActionResult go(string ac, string code, string tk, string ml)
+        {
+            THC_Library.Error error;
+            THC_Library.Reward.RewardConvertor reward;
+            string gender, age, Mobil, iid, addr;
+            int iLogKey;
+
+            Models.CodeRender codeRender = new Models.CodeRender();
+            bool bWin = codeRender.go(ac, code, tk, ml, out gender, out age,
+                                out Mobil, out iid, out addr, out reward, out iLogKey, out error);
+
+            if (error != null)
+            {
+                switch (error.Number)
+                {
+                    case THC_Library.CodeRenderException.ACTIVITY_FINISHED:
+                        ViewBag.Message = "活動已結束";
+                        ViewBag.MessageInfo = error.ErrorMessage;
+                        return View("outOfPage");              
+                      
+                    case THC_Library.CodeRenderException.ACTIVITY_NOT_START:
+                        ViewBag.Message = "活動尚未開始";
+                        ViewBag.MessageInfo = error.ErrorMessage;
+                        return View("outOfPage");
+
+                    case THC_Library.CodeRenderException.INVAILD_ACTIVITY:
+                        ViewBag.Message = "無效的活動";
+                        break;
+                    case THC_Library.CodeRenderException.INVAILD_CODE:
+                        ViewBag.Message = "無效的碼號";
+                        break;
+                    //return View("wrongPage");                       
+                    case THC_Library.CodeRenderException.REPEAT_SCAN:
+                        ViewBag.Message = "已掃描";
+                        string[] strSplit =  error.ErrorMessage.Split(' ');
+                        ViewBag.RepeatDate = strSplit[0];
+                        ViewBag.RepeatTime = strSplit[1];
+                        return View("repeatPage");
+                    case THC_Library.CodeRenderException.LOGIN_INVALID:
+                        ViewBag.Message = "無效的登入";
+                        break;
+                    case THC_Library.CodeRenderException.SYSTEM_ERROR:
+                        ViewBag.Message = "系統錯誤";
+                        break;                        
+                }
+                return View("wrongPage");
+            }
+            else
+            {              
+                if (bWin)
+                {                    
+                    ViewBag.ACTIVITY = ac;
+                    ViewBag.CODE = code;
+                    ViewBag.TICKET = tk;
+                    ViewBag.ACC = ml;
+                    ViewBag.MOBIL = Mobil;
+                    ViewBag.GENDER = gender;
+                    ViewBag.AGE = age;
+                    ViewBag.IID = iid;
+                    ViewBag.ADDRESS = addr;
+                    ViewBag.LOG_KEY = iLogKey.ToString();
+
+                    ViewBag.RWD_NAME = reward.RewardName;
+                    ViewBag.RWD_IMG = reward.RewardImage;
+                    ViewBag.RWD_COUPNUMBER = reward.CouponNumber;
+
+                    if(reward.RewardType == THC_Library.Reward.RewardType.ElectricCoupon)
+                    {
+                        if (reward is THC_Library.Reward.Edenred)
+                        {
+                            THC_Library.Reward.Edenred edenred = reward as THC_Library.Reward.Edenred;
+                            ViewBag.RWD_DATE = edenred.ValidPeriod;                            
+                        }     
+                        //虛擬獎項
+                        return View("rewardPage");
+                    }
+                    else
+                    {
+                        if (reward is THC_Library.Reward.Phyicalenred)
+                        {
+                            THC_Library.Reward.Phyicalenred phyenred = reward as THC_Library.Reward.Phyicalenred;
+                            ViewBag.RWD_DESC = phyenred.Description;
+                        }     
+                        //實體獎項
+                        return View("rewardPhyicalPage");
+                    }
+                    
+                }
+                else
+                {
+                    return View("tryAgain");
+                }               
+            }            
+        }
+                
+        public ActionResult get()
+        {
+            THC_Library.Error error;
+            var reqActivity = Request.QueryString["act"];
+            var reqCode = Request.QueryString["code"];
+            var reqTimestamp = Request.QueryString["tk"];
+            var reqAccount = Request.QueryString["ml"];
+            var reqMobil = Request.QueryString["m"];
+            var reqLogKey = Request.QueryString["lk"];
+            var reqCoupNumber = Request.QueryString["coup"];
+            var reqRwdType = Request.QueryString["type"];
+
+            Models.CodeRender codeRender = new Models.CodeRender();
+            bool bOK = codeRender.done(reqActivity, reqCode, reqTimestamp, reqAccount, reqCoupNumber, reqLogKey, out error);
+
+            if (error != null)
+            {                                
+                ViewBag.Message = error.ErrorMessage;
+                return View("wrongPage");
+            }
+            else
+            {
+                ViewBag.MOBIL = reqMobil;
+                ViewBag.RWD_COUPNUMBER = reqCoupNumber;
+
+                if (reqRwdType == "0")
+                {
+                    return View("get");
+                }
+                else if (reqRwdType == "1")
+                {
+                    return View("getPhyical");
+                }
+                else
+                {
+                    ViewBag.Message = "";
+                    ViewBag.MessageInfo = "";
+                    return View("outOfPage");
+                }
+            }
+           
+        }
+
+        public ActionResult phyicalReward()
+        {
+            return View("rewardPhyicalPage");
+        }
+
+        [HttpPost]
+        public JsonResult getCities()
+        {            
+            THC_Library.Error error;
+
+            Models.TaiwanAddress twAddr = new Models.TaiwanAddress();
+            DataTable addrTable = twAddr.getCities(out error);
+
+            string retJson;
+            if (error != null)
+            {
+                retJson = Newtonsoft.Json.JsonConvert.SerializeObject(error);
+            }
+            else
+            {
+                retJson = Newtonsoft.Json.JsonConvert.SerializeObject(addrTable);
+            }
+            return Json(retJson, "application/json", JsonRequestBehavior.AllowGet);
+        }
+               
+        [HttpPost]
+        public JsonResult getTowns(string city)
+        {
+            THC_Library.Error error;
+
+            Models.TaiwanAddress twAddr = new Models.TaiwanAddress();
+            DataTable addrTable = twAddr.getTown(city, out error);
+
+            string retJson;
+            if (error != null)
+            {
+                retJson = Newtonsoft.Json.JsonConvert.SerializeObject(error);
+            }
+            else
+            {
+                retJson = Newtonsoft.Json.JsonConvert.SerializeObject(addrTable);
+            }
+            return Json(retJson, "application/json", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult getRoad(string city, string town)
+        {
+            THC_Library.Error error;
+
+            Models.TaiwanAddress twAddr = new Models.TaiwanAddress();
+            DataTable addrTable = twAddr.getRoad(city, town, out error);
+
+            string retJson;
+            if (error != null)
+            {
+                retJson = Newtonsoft.Json.JsonConvert.SerializeObject(error);
+            }
+            else
+            {
+                retJson = Newtonsoft.Json.JsonConvert.SerializeObject(addrTable);
+            }
+            return Json(retJson, "application/json", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult doc(string code)
+        {
+            string retJson = "";
+
+            if (string.IsNullOrEmpty(code))
+            {
+                retJson = "{\"RESPONSE\" : \"ABORT\"}";
+                return Json(retJson, "application/json", JsonRequestBehavior.AllowGet);
+            }
+            if (code != "adDDFasF")
+            {
+                retJson = "{\"RESPONSE\" : \"ABORT\"}";
+                return Json(retJson, "application/json", JsonRequestBehavior.AllowGet);
+            }
+
+            IList<System.Data.SqlClient.SqlParameter> paraList = 
+                            new System.Collections.Generic.List<System.Data.SqlClient.SqlParameter>();
+            string strSQL = "update qr_record set QRC012=0,QRC013=NULL";
+            THC_Library.DataBase.DataBaseControl dbCtl = new THC_Library.DataBase.DataBaseControl();
+            try
+            {
+                dbCtl.Open();                
+                dbCtl.ExecuteCommad(strSQL, paraList);
+                retJson = "{\"RESPONSE\" : \"DONE\"}";
+            }
+            catch (Exception ex)
+            {
+                retJson = "{\"RESPONSE\" : \"ERROR\"}";
+            }
+            finally
+            {
+                dbCtl.Close();
+            }
+            return Json(retJson, "application/json", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
