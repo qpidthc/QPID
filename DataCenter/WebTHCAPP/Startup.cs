8ebd1713bb5e2d6ecc202c46ba50ca266fabad9c
qpidthc <qpid0421@gmail.com>
2017-08-30 10:52:31 +0800

    Add files via upload

---
+++ DataCenter/WebTHCAPP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTHCAPP.Startup))]
+namespace WebTHCAPP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
