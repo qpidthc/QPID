feee952736fa3d2c642a4a723e4bcc47ed4c374d
qpidthc <qpid0421@gmail.com>
2017-08-30 10:15:41 +0800

    Add files via upload

---
+++ ActivityFront/WebTHCEventUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebTHCEventUI.MvcApplication" Language="C#" %>
---
+++ ActivityFront/WebTHCEventUI/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿#define REMOTE_1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebTHCEventUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            THC_Library.DataBase.DataBaseControl.ResetDatabase("60.251.140.166", "THC_ACTIVITY");
+
+#if(REMOTE)
+          
+                 THC_Library.APPCURL.APP_URL = "http://60.251.140.166/WebTHCApp/";
+#else
+
+            THC_Library.APPCURL.APP_URL = "http://localhost:51323/";
+#endif
+
+        }
+    }
+}
