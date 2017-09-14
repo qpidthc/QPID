dc85c96c6e4e18a82945601161ddbffd5e9fcc34
qpidthc <qpid0421@gmail.com>
2017-09-15 01:36:07 +0800

    Add files via upload

---
+++ ActivityCodeGenerate/WebHTCBackEnd/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebHTCBackEnd
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            classes.ProductType.RefreshProductType();
+            classes.RewardType.RefreshRewardType();
+            classes.RewardVender.RefreshRewardVender();
+        }
+    }
+}
---
+++ ActivityCodeGenerate/WebHTCBackEnd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebHTCBackEnd.Startup))]
+namespace WebHTCBackEnd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
