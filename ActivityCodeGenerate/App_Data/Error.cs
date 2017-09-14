cd8724ae481c08d7ad76d8d31601385f97673b52
qpidthc <qpid0421@gmail.com>
2017-09-15 01:17:34 +0800

    Add files via upload

---
+++ ActivityCodeGenerate/App_Data/Error.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHTCBackEnd.Error
+{
+    public class Error
+    {        
+        public int Number
+        {
+            get;
+            set;
+        }
+        public string ErrorMessage
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ActivityCodeGenerate/App_Data/JsonResponeUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHTCBackEnd.Utility
+{
+    public class JsonResponeUtility
+    {
+        public static string ResultOK()
+        {
+            return "[{ \"result\" : 0 }]";
+        }
+    }
+}
