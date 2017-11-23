9b0052032e4c7e080c31f96ae693e02787baa04c
qpidthc <qpid0421@gmail.com>
2017-11-24 01:42:38 +0800

    Add files via upload

--- DataCenter/WebTHCAPP/Global.asax.cs
+++ DataCenter/WebTHCAPP/Global.asax.cs
@@ -23,5 +23,31 @@ namespace WebTHCAPP
             THC_Library.DataBase.DataBaseControl.ResetDatabase("60.251.140.166", "THC_CENTER");
 
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            //Do whatever you want to do with the error
+
+            //Show the custom error page...
+            Server.ClearError();
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error";
+
+            if ((Context.Server.GetLastError() is HttpException) && ((Context.Server.GetLastError() as HttpException).GetHttpCode() != 404))
+            {
+                routeData.Values["action"] = "Index";
+            }
+            else
+            {
+                //Handle 404 error and response code
+                Response.StatusCode = 404;
+                routeData.Values["action"] = "NotFound404";
+            }
+            Response.TrySkipIisCustomErrors = true;//If you are using IIS7, have this line
+            IController errorsController = new Controllers.ErrorController();
+            HttpContextWrapper wrapper = new HttpContextWrapper(Context);
+            var rc = new System.Web.Routing.RequestContext(wrapper, routeData);
+            errorsController.Execute(rc);
+        }
     }
 }
